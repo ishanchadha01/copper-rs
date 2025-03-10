e990ac65e77cf8fb5c232a8114373dc3a37b210d
ishan <ishanchadha01@gmail.com>
2025-03-10 06:10:03 -0700

    src and example build

--- components/sources/cu_dds_src/src/lib.rs
+++ components/sources/cu_dds_src/src/lib.rs
@@ -1,5 +1,43 @@
+use cu29::bincode::error::IntegerType;
 use cu29::clock::RobotClock;
 use cu29::prelude::*;
 use rustdds::*;
 use rustdds::no_key::{DataReader, DataWriter, DataSample}; // We use a NO_KEY topic here
 use serde::{Serialize, Deserialize};
+
+pub struct DDSSrc<P>
+where
+    P: CuMsgPayload + 'static,
+{
+    service_name: String,
+}
+
+impl<P> Freezable for DDSSrc<P> where P: CuMsgPayload {}
+
+impl<'cl, P> CuSrcTask<'cl> for DDSSrc<P>
+where
+    P: CuMsgPayload + 'cl + 'static,
+{
+    type Output = output_msg!('cl, P);
+
+    fn new(config: Option<&ComponentConfig>) -> CuResult<Self>
+    where
+        Self: Sized
+    {
+        Ok(Self {
+            service_name: "name".to_owned()
+        })
+    }
+
+    fn start(&mut self, _clock: &RobotClock) -> CuResult<()> {
+        Ok(())
+    }
+
+    fn process(&mut self, _clock: &RobotClock, new_msg: Self::Output) -> CuResult<()> {
+        Ok(())
+    }
+
+    fn stop(&mut self, _clock: &RobotClock) -> CuResult<()> {
+        Ok(())
+    }
+} impl CuSrcTask for DDSSrc<P>
